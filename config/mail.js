033fef0420c41f95e4bb282d938891adfe402ef9
anhtuankckt <anhtuankckt@gmail.com>
2024-05-16 10:05:56 +0700

    fix: change infomation email

--- config/mail.js
+++ config/mail.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: process.env.MAIL_HOST,
   port: process.env.MAIL_PORT,
-  secure: true,
+  secure: false,
   user: process.env.MAIL_USER,
   pass: process.env.MAIL_PASS
 }
